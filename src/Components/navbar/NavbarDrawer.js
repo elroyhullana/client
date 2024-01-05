f385c872963390438ea08ba8111ea7d763f4c12a
elroyhullana <hullanader23@gmail.com>
2024-01-05 20:06:21 +0800

    uwu

--- src/Components/navbar/NavbarDrawer.js
+++ src/Components/navbar/NavbarDrawer.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import { Drawer, IconButton, List, ListItem, ListItemText } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
-import Dialog from "@mui/material/Dialog";
 
 const pages = [
-  { label: "Recent Works", link: "/recentworks", color: "#372C2E" },
-  { label: "Contact", link: "/contact", color: "#372C2E" },
+  { label: "About", link: "#bio", color: "#372C2E" },
+  { label: "Recent Works", link: "#bodyContent", color: "#372C2E" },
+  { label: "Contact", link: "#contactMeArea", color: "#372C2E" },
 ];
 
 const linkStyle = {
@@ -25,15 +25,6 @@ const imageStyle = {
 
 const NavbarDrawer = () => {
   const [openDrawer, setOpenDrawer] = useState(false);
-  const [openDialog, setOpenDialog] = useState(false);
-
-  const handleOpenDialog = () => {
-    setOpenDialog(true);
-  };
-
-  const handleCloseDialog = () => {
-    setOpenDialog(false);
-  };
 
   return (
     <React.Fragment>
@@ -47,71 +38,13 @@ const NavbarDrawer = () => {
 
           {pages.map((page, index) => (
             <ListItem onClick={() => setOpenDrawer(false)} key={index}>
-              {page.label === "Contact" ? (
-                <ListItemText primary={page.label} onClick={handleOpenDialog} style={{ textStyle }} />
-              ) : (
-                <a href={page.link} style={{ ...linkStyle }}>
-                  <ListItemText primary={page.label} style={{ textStyle }} />
-                </a>
-              )}
+              <a href={page.link} style={{ ...linkStyle }}>
+                <ListItemText primary={page.label} style={{ textStyle }} />
+              </a>
             </ListItem>
           ))}
         </List>
       </Drawer>
-
-      <Dialog open={openDialog} onClose={handleCloseDialog}>
-        <div style={{ padding: "20px", textAlign: "center" }}>
-          <h1 style={{ fontSize: "24px", marginBottom: "20px" }}>Contact Me</h1>
-          <div style={{ marginBottom: "15px" }}>
-            <a href='https://m.facebook.com/ElroyHullana23/' target="_blank" rel="noopener noreferrer">
-              <img
-                src="/icons/facebook.png"
-                alt="Facebook"
-                style={{ width: "40px", marginRight: "10px" }}
-              />
-            </a>
-
-            <a href='https://www.instagram.com/elroy23hullana/?hl=en' target="_blank" rel="noopener noreferrer">
-              <img
-                src="/icons/instagram.png"
-                alt="Instagram"
-                style={{ width: "40px", marginRight: "10px" }}
-              />
-            </a>
-
-            <a href='https://www.linkedin.com/in/elroyhullana?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app' target="_blank" rel="noopener noreferrer">
-              <img
-                src="/icons/linkedin.png"
-                alt="LinkedIn"
-                style={{ width: "40px", marginRight: "10px" }}
-              />
-            </a>
-
-            <a href='mailto:hullanader23@gmail.com'>
-              <img
-                src="/icons/gmail-logo.png"
-                alt="Gmail"
-                style={{ width: "40px" }}
-              />
-            </a>
-          </div>
-          <button
-            onClick={handleCloseDialog}
-            style={{
-              padding: "10px 20px",
-              fontSize: "16px",
-              fontWeight: "bold",
-              backgroundColor: "#DE9E48",
-              color: "white",
-              border: "none",
-              borderRadius: "5px",
-              cursor: "pointer",
-            }}
-          >
-            Close
-          </button>
-        </div>
-      </Dialog>
 
       <IconButton
         sx={{ color: "#DE9E48", marginLeft: "70px" }}
